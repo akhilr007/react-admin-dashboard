b0ec2f4b7d13aac903227f44db8279c0211b1bc7
Akhil Rajan <rajanakhil07@gmail.com>
2024-02-02 21:54:27 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -12,6 +14,7 @@ function App() {
         <div className="app">
           <main className="content">
             <Topbar />
+            <Outlet />
           </main>
         </div>
       </ThemeProvider>
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,71 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import Topbar from "./scenes/global/Topbar.jsx";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
   },
+
+  // {
+  //   path: "/team",
+  //   element: <Team />,
+  // },
+  // {
+  //   path: "/contacts",
+  //   element: <Contacts />,
+  // },
+  // {
+  //   path: "/invoices",
+  //   element: <Invoices />,
+  // },
+  // {
+  //   path: "/form",
+  //   element: <Form />,
+  // },
+  // {
+  //   path: "/bar",
+  //   element: <Bar />,
+  // },
+  // {
+  //   path: "pie",
+  //   element: <Pie />,
+  // },
+  // {
+  //   path: "/line",
+  //   element: <Line />,
+  // },
+  // {
+  //   path: "/faq",
+  //   element: <FAQ />,
+  // },
+  // {
+  //   path: "/calendar",
+  //   element: <Calendar />,
+  // },
+  // {
+  //   path: "/geography",
+  //   element: <Geography />,
+  // },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
