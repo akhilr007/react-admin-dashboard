b273a16cc9f1a15cfb8f6dccf2fd46fdaf620ac4
Akhil Rajan <rajanakhil07@gmail.com>
2024-02-02 21:16:12 +0530

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar.jsx";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <Topbar />,
+    element: <App />,
   },
 ]);
 
