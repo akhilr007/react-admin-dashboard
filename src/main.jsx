c2f9bea6c4cf5f6cfaca3777ec53122788feaa89
Akhil Rajan <rajanakhil07@gmail.com>
2024-02-02 20:41:17 +0530

    added theme for light and dark

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar.jsx";
 
-const routes = createBrowserRouter([{}]);
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Topbar />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
